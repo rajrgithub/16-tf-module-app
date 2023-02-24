5c176f98edb464f4a00fbec804818f37d4bd96e1
brajasekharreddy <raj479.mits@gmail.com>
2023-02-24 23:15:01 +0530

    update policy to get nexus credentials

--- main.tf
+++ main.tf
@@ -46,7 +46,10 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:973130779128:parameter/${var.env}.${var.component}*"
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:973130779128:parameter/${var.env}.${var.component}*",
+          "arn:aws:ssm:us-east-1:973130779128:parameter/nexus*"
+        ]
       },
       {
         "Sid" : "VisualEditor1",
